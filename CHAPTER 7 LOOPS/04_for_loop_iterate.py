49a5db5cd8d2b8cb39c077b88612139bd288ee8d
FarhanAlam-hub <justinfarhan786@gmail.com>
2026-09-03 18:41:05 +0530

    printing Element using for_Loop

---
+++ CHAPTER 7 LOOPS/04_for_loop_iterate.py
@@ -0,0 +1,19 @@
+#    ********printing elements of LIST using for_Loop*********
+
+l = [1,2,3,4,"farhan",3.14]
+for i in l:
+    print(i)
+
+
+#    ********printing elements of TUPLES using for_Loop*********
+
+t = (10,12,133,"farhan",True)
+for i in t:
+    print(i)
+
+
+#    ********printing elements of STRING using for_Loop*********
+
+s = "FARHAN"
+for i in s:
+    print(i)
