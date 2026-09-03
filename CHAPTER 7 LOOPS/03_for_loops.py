8df89afe77f737338c5eb7ab9a0967f391e96d52
FarhanAlam-hub <justinfarhan786@gmail.com>
2026-09-03 18:28:22 +0530

    print_no._using_forLoop

---
+++ CHAPTER 7 LOOPS/03_for_loops.py
@@ -0,0 +1,14 @@
+L = [1,2,3,4]
+for item in L :  #SImple use of FOR LOOPS
+    print(item)
+
+#          ********RANGE FUNCTION*****
+
+for i in range(0,7):
+    print(i)
+
+
+#          ********RANGE FUNCTION*****range(start, end, step_size)******
+
+for i in range(5, 55, 5):
+    print(i)
