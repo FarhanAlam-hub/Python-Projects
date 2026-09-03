9c7c311cc3b82d25524d6ee6e98b65eced1a6117
FarhanAlam-hub <justinfarhan786@gmail.com>
2026-09-03 23:47:20 +0530

    Question_01

--- CHAPTER 7 LOOPS/practice set/01_name_list.py
+++ CHAPTER 7 LOOPS/practice set/01_name_list.py
@@ -1,3 +1,5 @@
+# Question No.1 ----->Write a program to greet all the person names stored in a list 'l' and which starts
+#with F.
 l =["Farhan","Faizan","Cizaan","Fatima","Kaif"]
 
 for name in l:
