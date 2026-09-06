7d589a447f53ff169b8a51fa90d7c3ad5067fde2
FarhanAlam-hub <justinfarhan786@gmail.com>
2026-09-06 23:16:21 +0530

    Area of rectangle

--- CHAPTER 8 FUNCTION/Area_of_rectangle.py
+++ CHAPTER 8 FUNCTION/Area_of_rectangle.py
@@ -1,8 +1,9 @@
 # Q.2 --> Area of a Rectangle
-def square(l,b):
-    print(l*b)
+def area():
+    area = l*b
+    print(area)
 l = int(input("Enter Length of Rectangle:"))
 b = int(input("Enter Breadth of Rectangle:"))
 
 
-square(l,b)
+area()
