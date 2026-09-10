47f0f33940833e1e8c2980b90b2f586e1d218cac
FarhanAlam-hub <justinfarhan786@gmail.com>
2026-09-10 23:31:57 +0530

    init Constructor

---
+++ CHAPTER 10 OOPS/05_constructor.py
@@ -0,0 +1,20 @@
+class Students:
+    name = "Farhan"
+    language = "Python"
+    roll = 2430277
+
+    def __init__(self,name,roll,language):   # dunder method which is automatically called
+        self.name = name
+        self.roll = roll
+        self.language = language
+
+
+    def getroll(self):
+        print(f"Your roll no. is:{self.roll}\nYour language is:{self.language} ")
+    def greet(self):
+        print(f"Good moring, {self.name}")
+
+farhan = Students("Farhan", 1224071,"JavaScript")
+farhan.greet()
+farhan.getroll()
+# print(farhan.name,farhan.roll)
