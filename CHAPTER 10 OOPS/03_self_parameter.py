5c628213e4796421cb4cd02f26ff2512867d2cac
FarhanAlam-hub <justinfarhan786@gmail.com>
2026-09-10 17:30:28 +0530

    Self Parameter

---
+++ CHAPTER 10 OOPS/03_self_parameter.py
@@ -0,0 +1,10 @@
+class Students:
+    name = "Farhan"
+    language = "Python"
+    roll = 2430277
+    def getroll(self):
+        print(f"Your roll no. is:{self.roll}\nYour language is:{self.language} ")
+
+farhan = Students()
+farhan.getroll()
+# print(farhan.name,farhan.roll)
