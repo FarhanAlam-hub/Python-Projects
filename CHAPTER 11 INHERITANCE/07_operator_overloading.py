49db239f58f82e53ab1db717ae2cbac42d404b7c
FarhanAlam-hub <justinfarhan786@gmail.com>
2026-09-15 00:08:09 +0530

    Operator Overloading

--- CHAPTER 11 INHERITANCE/07_operator_overloading.py
+++ CHAPTER 11 INHERITANCE/07_operator_overloading.py
@@ -10,10 +10,19 @@ class number:
     
     def __mul__(self, num):
         return self.n * num.n
+    
+    def __truediv__(self, num):
+            return self.n / num.n
 
-n = number(4)
+    def __floordiv__(self, num):
+                return self.n // num.n
+    
+
+n = number(6)
 m = number(2)
 
-print(n + m)
-print(n - m)
-print(n * m)
+print(f"The sum is: {n + m}")
+print(f"The difference is: {n - m}")
+print(f"The product is: {n * m}")
+print(f"The division is: {n / m}")
+print(f"The floor_Division is: {n // m}")
