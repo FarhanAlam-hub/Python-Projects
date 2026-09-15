2c38278dc9afa5663ccf41499a82e533c5c05c36
FarhanAlam-hub <justinfarhan786@gmail.com>
2026-09-15 23:13:40 +0530

    Problem 3

---
+++ CHAPTER 11 INHERITANCE/Practice Set/03_problem3.py
@@ -0,0 +1,18 @@
+class employee:
+    salary = 234
+    increment = 20
+
+    @property
+    def salaryAfterincrement(self):
+        return (self.salary + self.salary * (self.increment/100))
+
+# new salary = old salary (1 + increment/100)
+
+    @salaryAfterincrement.setter
+    def salaryAfterincrement(self, salary):
+        self.increment = ((salary / self.salary) - 1 )*100
+
+e = employee()
+print(e.salaryAfterincrement)
+e.salaryAfterincrement = 280.8
+print(e.increment)
