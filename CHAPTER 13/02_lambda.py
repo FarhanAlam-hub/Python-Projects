527bafb2bdecd79f8454d878d570c7f63e94df7e
FarhanAlam-hub <justinfarhan786@gmail.com>
2026-09-18 23:28:32 +0530

    Lambda Function

--- CHAPTER 13/02_lambda.py
+++ CHAPTER 13/02_lambda.py
@@ -2,5 +2,10 @@
 #     return n*n
 
 square = lambda n: n*n
+print(f"Square is: {square(5)}")
 
-print(square(5))
+sum = lambda a,b,c : a+b+c
+print(f"The sum is: {sum(1,2,3)}")
+
+mul = lambda a,b : a*b
+print(f"The product is: {mul(2,3)}")
