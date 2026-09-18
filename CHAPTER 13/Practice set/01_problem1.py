faf9b41ce1b71330bda1694e222321c01c06b7e9
FarhanAlam-hub <justinfarhan786@gmail.com>
2026-09-19 00:02:15 +0530

    problem1

---
+++ CHAPTER 13/Practice set/01_problem1.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+marks = int(input("Enter your marks: "))
+rollNumber = int(input("Enter your rollNumber: "))
+
+s1 = "The name of the student is {} his marks is {} and phone number is {} ".format(name,marks,rollNumber)
+
+print(s1)
