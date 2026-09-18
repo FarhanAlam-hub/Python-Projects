e55b6851642f1b40d7cc3250a7111e5bb6ddb6b7
FarhanAlam-hub <justinfarhan786@gmail.com>
2026-09-18 23:39:54 +0530

    Format

---
+++ CHAPTER 13/04_format.py
@@ -0,0 +1,6 @@
+a = "{0} is a good {1}".format("Farhan","coder")
+b = "{1} is a good {0}".format("Farhan","coder")
+print (a)
+print(b)
+
+#.format() was mainly used when there was no f string in python now it is not use that much 
